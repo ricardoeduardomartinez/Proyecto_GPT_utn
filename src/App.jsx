56e9036fa0e45d7b7fbaffbd53afabc3bdbd9b0c
Ricardo Martinez <ricardoeduardomartinez@gmail.com>
2024-09-17 17:59:48 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function App() {
+
+  const ArrayDeConversaciones=[{
+                                  Id: 1,
+                                  Fecha: 12/9,
+                                  Conversacion: [{
+                                                  Titulo:"Titulo",  
+                                                  Chat:"hola como estas?"
+                              }]
+  }]
+  
+
+  return (
+    <div>
+      hola
+    </div>
+  )
+}
+
+export default App
